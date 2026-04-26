e8d9426ba20ddfd3935a976ca6312788b9877614
norika <norika1207@gmail.com>
2026-04-26 16:30:32 +0800

    Initial Commit

---
+++ Alfred/AlfredApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlfredApp.swift
+//  Alfred
+//
+//  Created by Norika Oda on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AlfredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
